d37e9210fcc0cb97f1a66ccd34e93da746ecc0cc
zulfa144 <zulfaaulia4019@gmail.com>
2025-08-21 15:39:41 +0700

    Add files via upload

---
+++ edit-siswa.php
@@ -0,0 +1,65 @@
+<?php 
+  
+  include('koneksi.php');
+  
+  $id = $_GET['id'];
+  
+  $query = "SELECT * FROM tbl_siswa WHERE id_siswa = $id LIMIT 1";
+
+  $result = mysqli_query($connection, $query);
+
+  $row = mysqli_fetch_array($result);
+
+  ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Edit Siswa</title>
+  </head>
+
+  <body>
+
+    <div class="container" style="margin-top: 80px">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <div class="card">
+            <div class="card-header">
+              EDIT SISWA
+            </div>
+            <div class="card-body">
+              <form action="update-siswa.php" method="POST">
+                
+                <div class="form-group">
+                  <label>NISN</label>
+                  <input type="text" name="nisn" value="<?php echo $row['nisn'] ?>" placeholder="Masukkan NISN Siswa" class="form-control">
+                  <input type= "hidden" name="id_siswa" value="<?php echo $row['id_siswa'] ?>">
+                </div>
+
+                <div class="form-group">
+                  <label>Nama Lengkap</label>
+                  <input type="text" name="nama_lengkap" value="<?php echo $row['nama_lengkap'] ?>" placeholder="Masukkan Nama Siswa" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Alamat</label>
+                  <textarea class="form-control" name="alamat" placeholder="Masukkan Alamat Siswa" rows="4"><?php echo $row['alamat'] ?></textarea>
+                </div>
+                
+                <button type="submit" class="btn btn-success">UPDATE</button>
+                <button type="reset" class="btn btn-warning">RESET</button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ simpan-siswa.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Tambah Siswa</title>
+  </head>
+
+  <body>
+
+    <div class="container" style="margin-top: 80px">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <div class="card">
+            <div class="card-header">
+              TAMBAH SISWA
+            </div>
+            <div class="card-body">
+              <form action="simpan-siswa.php" method="POST">
+                
+                <div class="form-group">
+                  <label>NISN</label>
+                  <input type="text" name="nisn" placeholder="Masukkan NISN Siswa" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Nama Lengkap</label>
+                  <input type="text" name="nama_lengkap" placeholder="Masukkan Nama Siswa" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Alamat</label>
+                  <textarea class="form-control" name="alamat" placeholder="Masukkan Alamat Siswa” rows="4"></textarea>
+                </div>
+                
+                <button type="submit" class="btn btn-success">SIMPAN</button>
+                <button type="reset" class="btn btn-warning">RESET</button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+
+<form action="simpan-siswa.php" method="POST">
+
+
+
+<?php
+
+//include koneksi database
+include('koneksi.php');
+
+//get data dari form
+$nisn           = $_POST['nisn'];
+$nama_lengkap = $_POST['nama_lengkap'];
+$alamat       = $_POST['alamat'];
+
+//query insert data ke dalam database
+$query = "INSERT INTO tbl_siswa (nisn, nama_lengkap, alamat) VALUES ('$nisn', '$nama_lengkap', '$alamat')";
+
+//kondisi pengecekan apakah data berhasil dimasukkan atau tidak
+if($connection->query($query)) {
+
+    //redirect ke halaman index.php 
+    header("location: index.php");
+
+} else {
+
+    //pesan error gagal insert data
+    echo "Data Gagal Disimpan!";
+
+}
+
+?>
